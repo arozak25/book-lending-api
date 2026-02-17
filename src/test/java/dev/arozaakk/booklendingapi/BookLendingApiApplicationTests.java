8083aa34a56f9b4c71d45d96e305c828e8307772
Abdul Rozak <arozak25@gmail.com>
2026-02-17 22:38:21 +0700

    Init project

---
+++ src/test/java/dev/arozaakk/booklendingapi/BookLendingApiApplicationTests.java
@@ -0,0 +1,11 @@
+package dev.arozaakk.booklendingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLendingApiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
